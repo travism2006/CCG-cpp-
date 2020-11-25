73ae131d0c2d6fc87d01354d3f3d2a8d632f97c7
Travis Mitchum <travism2006@gmail.com>
2020-11-25 09:30:28 -0500

    Modes src and header
    Added src and header files for Modes module.

---
+++ Modes.cpp
@@ -0,0 +1,7 @@
+/*
+ * Modes.cpp
+ *
+ *  Created on: Nov 18, 2020
+ *  Author: Travis Mitchum
+ */
+
---
+++ Modes.h
@@ -0,0 +1,22 @@
+/*
+ * Modes.h
+ *
+ *  Created on: Nov 18, 2020
+ *  Author: Travis Mitchum
+ */
+
+namespace CCG
+{
+//	namespace Modes
+//	{
+//
+//
+//		class FileSecurityModes
+//		{
+//		private:
+//			enum FileModes{Allowed, Unallowed};
+//		public:
+//			//
+//		};
+//	}
+}
