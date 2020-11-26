8bfd3d23e5efbc25115312ca78eede96568d7267
idledeity <idledeity@jumpjets.net>
2020-11-25 18:27:15 -0600

    code review

--- AddCmd.cpp
+++ AddCmd.cpp
@@ -10,8 +10,19 @@
 
 namespace CCG
 {
+	 /* 
+	  * The `enum` keyword is not needed here. I've never actually seen this, althugh I suppose it compiles
+	  * 
+	  * Also, this function seems more like its returning the name not the value??
+	  * AddCmd::getFlagName(AddCmd::ClassFlag clsFlag);
+	  * 
+	  * Yes, a bit of a stretch/nit-pick but names really do matter when programs get larger and more complex
+	  */
 	std::string AddCmd::getFlagValue(enum AddCmd::ClassFlag clsFlag)
 	{
+	   /*
+		 * You technically don't need the scope AddCmd:: here since you are inside a member function of this class
+		 */
 		if(clsFlag == AddCmd::SHORT_FLAG)
 			{return "-cls";}
 		else if(clsFlag == AddCmd::LONG_FLAG)
--- AddCmd.h
+++ AddCmd.h
@@ -12,9 +12,41 @@ namespace CCG
 	class AddCmd
 	{
 	private:
+		 /*
+		  * 1. This declares a member 'addClsFlag' that is never referenced anywhere except the constructor()
+		  * 2. You've declared this enumeration `private` which means no code* external to the class can use
+		  *    it. But you've exposed it via a public member, getFlagVaue() which as written cannot be used
+		  *    externally with SHORT_FLAG or LONG_FLAG
+		  * 3. As of C++11 with improvements in C++14, NSDMI (Non-static data member initialization) is allowed
+		  *    and should be used where possible. Plus it's just way more convenient. In short, you can just
+		  *    do:
+		  * 
+		  *    ClassFlag addClsFlag = SHORT_FLAG; 
+		  *
+		  *    This saves you from "forgetting" to initalize your member in a separate constructor, and makes
+		  *    it handy for anyone reading your code to see the member with its default value all right together
+		  *    here in the header
+		  *
+		  *    Further, in this example usage, using NSDMI would entirely negate the need for defining a custom
+		  *    constructor entirely. 
+		  *  4. In general, `enum class` should be preferred over c-style enums as they have a few added benefits.
+		  */
 		enum ClassFlag{SHORT_FLAG, LONG_FLAG} addClsFlag;
 	public:
 		AddCmd();
+		
+		/*
+		 * Unclear if this is intended or not, but this function as written does not operate on any member of the
+		 * AddCmd class. As such, you could have declared it as `static` which means its available to be called
+		 * without having an actual object of type AddCmd.
+		 * 
+		 * class AddCmd {
+		 *    static std::string getFlagValue(ClassFlag);
+		 * };
+		 * 
+		 * // some other code
+		 * AddCmd::getFlagValue(SHORT_FLAG)
+		 */
 		std::string getFlagValue(enum ClassFlag);
 	};
 }
